4a5086b498f303260ab5f6caff9fa8bb08ee0d50
Alexander <cracked87fn@gmail.com>
2022-12-01 19:35:41 +0200

    new feature

--- fortnite-api-server/routes/events/list.js
+++ fortnite-api-server/routes/events/list.js
@@ -8,6 +8,24 @@ const list = async (req, res) => {
     const data = await eventList(lang, region)
     if(data === false) return res.json({result: false, error: 'FortniteAPI is not responding'})
 
+    if(req.query.last !== undefined) {
+        let last = 20
+
+        if(!isNaN(req.query.last)) {
+            last = req.query.last < 100 ? req.query.last : last
+        }
+
+        data.events = data.events.sort(function(a,b){
+            // Turn your strings into dates, and then subtract them
+            // to get a value that is either negative, positive, or zero.
+            return new Date(b.endTime) - new Date(a.endTime);
+          });
+
+        
+        if(data.events.length >= last)
+        data.events = data.events.slice(0, last)
+    }
+
     if(req.query.upcomingEvents == 'true') {
         data.events = data.events.filter(item => new Date(item.endTime) > new Date())
     }

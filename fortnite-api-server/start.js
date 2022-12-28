9a266d236a7e839adb92049952342122d4e0b8fe
Alexander <cracked87fn@gmail.com>
2022-12-28 19:44:55 +0200

    CORS http + https support

--- fortnite-api-server/start.js
+++ fortnite-api-server/start.js
@@ -9,21 +9,6 @@ app.enable('trust proxy');
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
-const cors = {
-  origin: [process.env.CLIENT_DOMAIN_NAME,
-          `http://${process.env.CLIENT_DOMAIN_NAME}`,
-          `https://${process.env.CLIENT_DOMAIN_NAME}`],
-  default: process.env.CLIENT_DOMAIN_NAME
-}
-
-app.all('*', function(req, res, next) {
-  const origin = cors.origin.includes(req.get('host').toLowerCase()) ? req.headers.origin : cors.default;
-  res.header("Access-Control-Allow-Origin", origin);
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Credentials: true");
-  next();
-});
-
 app.use('/', router)
 app.get('*', (req, res) => {
   res.json({result: false, code: 'NOT_FOUND'})
--- main-api-server/start.js
+++ main-api-server/start.js
@@ -4,7 +4,6 @@ require('./main/database/start.js')
 
 const app = express();
 app.enable('trust proxy');
-app.set('subdomain offset', 2);
 
 var superNumber = 1
 app.use((req, res, next) => {
@@ -23,28 +22,6 @@ app.use(bodyParser.json());
 
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
-
-const cors = {
-  origin: [process.env.CLIENT_DOMAIN_NAME,
-          `http://${process.env.CLIENT_DOMAIN_NAME}`,
-          `https://${process.env.CLIENT_DOMAIN_NAME}`,
-          `http://www.${process.env.CLIENT_DOMAIN_NAME}`,
-          `https://www.${process.env.CLIENT_DOMAIN_NAME}`
-        ]
-}
-
-app.all('*', function(req, res, next) {
-  const origin = cors.origin.includes(req.get('host').toLowerCase()) ? req.get('host').toLowerCase() : "null";
-  console.log({cors})
-  console.log(req.protocol + '://' + req.get('host'))
-  console.log({secure: req.secure})
-  res.header("Access-Control-Allow-Origin", req.protocol + '://' + origin);
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Sec-Fetch-Site", "same-origin");
-  next();
-});
-
 
 app.use('/', require('./routes/routes'))
 
